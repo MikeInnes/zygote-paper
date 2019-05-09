c2571e7349a3a6ab631eeb5b7c075cb9008e611c
Elliot Saba <staticfloat@gmail.com>
2019-05-09 09:11:47 -0700

    Update `run_examples.sh` to only try running files that exist

--- run_examples.sh
+++ run_examples.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 # Ensure that all examples can be run directly
-for subdir in *_*_*; do
-    julia --project=. --color=yes ${subdir}/*.jl
+for subdir in ${DIR}/*_*_*; do
+    script=$(echo ${subdir}/*.jl)
+    if [[ -f ${script} ]]; then
+        julia --project=. --color=yes "${script}"
+    fi
 done
