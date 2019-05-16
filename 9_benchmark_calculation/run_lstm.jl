61dcde38412d5cf362a7775b2ad8bc4762708bc5
Elliot Saba <staticfloat@gmail.com>
2019-05-16 16:56:42 -0700

    Fix stupid bugs, do larger parameter sweep

--- 9_benchmark_calculation/run_lstm.jl
+++ 9_benchmark_calculation/run_lstm.jl
@@ -1,4 +1,5 @@
-using Zygote, Flux, BenchmarkTools, Plots, Test
+using Zygote, Flux, BenchmarkTools, Test, DataFrames, GLM, Statistics
+using Plots
 
 function build_model(num_layers::Int, size::Int)
     layers = Any[]
@@ -8,15 +9,12 @@ function build_model(num_layers::Int, size::Int)
     return mapleaves(Flux.data, Chain(layers...))
 end
 
-timings = Dict()
-layers = 1:5
-seq_lens = 1:20
-for num_layers in layers
-    timings[num_layers] = Any[]
-    for seq_len in seq_lens
-        size = 4
-        model = build_model(num_layers, size)
-        x = randn(size)
+@info "Reporting (num_layers, seq_len, feature_size, batch_size, time)"
+function sweep_batchsizes(batch_sizes, num_layers = 1, seq_len = 4, feature_size = 4)
+    timings = Float64[]
+    for batch_size in batch_sizes
+        model = build_model(num_layers, feature_size)
+        x = randn(feature_size, batch_size)
         y, back = Zygote.forward(m -> begin
             x_t = x
             for idx in 1:seq_len
@@ -24,24 +22,35 @@ for num_layers in layers
             end
             return sum(x_t)
         end, model)
-        push!(timings[num_layers], @benchmark $back(1f0))
-        @info num_layers, seq_len, size, timings[num_layers][end]
+        push!(timings, minimum(@benchmark $back(1f0)).time/(seq_len*num_layers))
+        @info num_layers, seq_len, feature_size, batch_size, timings[end]
     end
+    return timings
 end
 
-using DataFrames, GLM, Statistics
-overhead_estimates = Float64[]
-for num_layers in layers
-    data = DataFrame(X=[minimum(t).time for t in timings[num_layers]], Y=seq_lens)
+
+batch_sizes = 1:4
+timings = Dict()
+for num_layers in 1:3,
+    seq_len in (4, 8),
+    feature_size in (4, 8)
+
+    timings[(num_layers, seq_len, feature_size, batch_sizes)] = sweep_batchsizes(batch_sizes, num_layers, seq_len, feature_size)
+end
+
+overhead_estimates = Dict()
+for k in keys(timings)
+    data = DataFrame(X=batch_sizes, Y=timings[k])
     ols = lm(@formula(Y ~ X), data)
-    push!(overhead_estimates, coeftable(ols).cols[1][1])
+    overhead_estimates[k] = coeftable(ols).cols[1][1]
 end
-println("Mean overhead: $(mean(overhead_estimates))ns")
+@show overhead_estimates
+println("Mean overhead: $(mean(values(overhead_estimates)))ns")
 
-gr()
-ENV["GKSwstype"]="100"
-p = plot()
-for num_layers in layers
-    plot!(p, seq_lens, [minimum(t).time for t in timings[num_layers]]; title="LSTM Runtime", ylabel="Runtime (μs)", xlabel="Sequence Length", label="$(num_layers) layers")
-end
-savefig(p, joinpath(@__DIR__, "lstm_runtime.png"))
+#gr()
+#ENV["GKSwstype"]="100"
+#p = plot()
+#for num_layers in layers
+#    plot!(p, seq_lens, [minimum(t).time for t in timings[num_layers]]; title="LSTM Runtime", ylabel="Runtime (μs)", xlabel="Sequence Length", label="$(num_layers) layers")
+#end
+#savefig(p, joinpath(@__DIR__, "lstm_runtime.png"))
