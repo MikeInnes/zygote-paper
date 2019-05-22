a6a321c4a78f605997ad94a9840a80147cf9ba8e
Christopher Rackauckas <accounts@chrisrackauckas.com>
2019-05-21 22:55:24 -0400

    neural sde plotting

--- neural_sde/neural_sde.jl
+++ neural_sde/neural_sde.jl
@@ -1,4 +1,4 @@
-using Flux, DiffEqFlux, DifferentialEquations, Plots
+using Flux, DiffEqFlux, StochasticDiffEq, Plots, DiffEqMonteCarlo
 
 u0 = Float32[2.; 0.]
 datasize = 30
@@ -14,13 +14,18 @@ function true_noise_func(du,u,p,t)
     du .= mp.*u
 end
 prob = SDEProblem(trueODEfunc,true_noise_func,u0,tspan)
-sde_data = Array(solve(prob,SOSRI(),saveat=t))
+
+# Take a typical sample from the mean
+monte_prob = MonteCarloProblem(prob)
+monte_sol = solve(monte_prob,SOSRI(),num_monte = 100)
+monte_sum = MonteCarloSummary(monte_sol)
+sde_data = Array(timeseries_point_mean(monte_sol,t))
 
 dudt = Chain(x -> x.^3,
              Dense(2,50,tanh),
              Dense(50,2))
 ps = Flux.params(dudt)
-n_sde = x->neural_dmsde(dudt,x,mp,tspan,SOSRI(),saveat=t,reltol=1e-2,abstol=1e-2)
+n_sde = x->neural_dmsde(dudt,x,mp,tspan,SOSRI(),saveat=t,reltol=1e-1,abstol=1e-1)
 
 pred = n_sde(u0) # Get the prediction using the correct initial condition
 scatter(t,sde_data[1,:],label="data")
@@ -29,10 +34,11 @@ scatter!(t,Flux.data(pred[1,:]),label="prediction")
 function predict_n_sde()
   n_sde(u0)
 end
-loss_n_sde() = sum(abs2,sde_data .- predict_n_sde())
-Flux.back!(loss_n_sde())
+loss_n_sde1() = sum(abs2,sde_data .- predict_n_sde())
+loss_n_sde10() = sum([sum(abs2,sde_data .- predict_n_sde()) for i in 1:10])
+Flux.back!(loss_n_sde1())
 
-data = Iterators.repeated((), 100)
+data = Iterators.repeated((), 10)
 opt = ADAM(0.025)
 cb = function () #callback function to observe training
   sample = predict_n_sde()
@@ -48,4 +54,18 @@ end
 # Display the SDE with the initial parameter values.
 cb()
 
-Flux.train!(loss_n_sde, ps, data, opt, cb = cb)
+Flux.train!(loss_n_sde1 , ps, Iterators.repeated((), 100), opt, cb = cb)
+Flux.train!(loss_n_sde10, ps, Iterators.repeated((), 5), opt, cb = cb)
+
+dudt_(u,p,t) = Flux.data(dudt(u))
+g(u,p,t) = mp.*u
+nprob = SDEProblem(dudt_,g,u0,(0.0f0,1.2f0),nothing)
+
+monte_nprob = MonteCarloProblem(nprob)
+monte_nsol = solve(monte_nprob,SOSRI(),num_monte = 100)
+monte_nsum = MonteCarloSummary(monte_nsol)
+plot(monte_nsol,color=1,alpha=0.3)
+plot(monte_nsum)
+plot!(t,sde_data',lw=3)
+savefig("neural_sde.pdf")
+savefig("neural_sde.png")
